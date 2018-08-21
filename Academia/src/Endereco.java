2ef99d36cf4e27a67a468a40fff24e608e9ecd08
Alisson Victor <alyssonvictor00@gmail.com>
2018-08-20 21:27:56 -0300

    Classe endereço adicionada

---
+++ Academia/src/Endereco.java
@@ -0,0 +1,73 @@
+package academia.ufpb.br;
+
+public class Endereco {
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+
+	public Endereco(){}
+
+	public Endereco(String logradouro, String numero, String complemento, String bairro, String cidade, String estado){
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+	
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
+	public String getLogradouro() {
+		return this.logradouro;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getNumero() {
+		return this.numero;
+	}
+	
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	public String getComplemento() {
+		return this.complemento;
+	}
+	
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	public String getBairro() {
+		return this.bairro;
+	}
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	public String getCidade() {
+		return this.cidade;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public String getEstado() {
+		return this.estado;
+	}
+	
+	public String toString() {
+		return "Logradouro: "+this.logradouro+" nº: "+this.numero+"Bairro: "+this.bairro+"Cidade: "+this.cidade+"Estado: "+this.estado+"Complemento: "+this.complemento;
+	}
+}
