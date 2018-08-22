d34e647b652c3b2222b703365e0a1770eca7664b
HeitorMOliveira <33163610+HeitorMOliveira@users.noreply.github.com>
2018-08-22 14:09:58 -0300

    Adicionando método que retorna o nome do plano selecionado.

--- Academia/src/Plano.java
+++ Academia/src/Plano.java
@@ -1,46 +1,74 @@
 public class Plano {
 
-    private int plano;
-    private double valor;
+	private int plano;
+	private double valorPlano;
 
-    private final static double PLANO_FAMILIA = 130;
-    private final static double PLANO_UNIVERSITARIO = 40;
-    private final static double PLANO_PADRAO = 60;
-    private final static double PLANO_NATACAO = 100;
-    private final static double PLANO_LUTA = 80;
+	private final static double PLANO_FAMILIA = 130;
+	private final static double PLANO_UNIVERSITARIO = 40;
+	private final static double PLANO_PADRAO = 60;
+	private final static double PLANO_NATACAO = 100;
+	private final static double PLANO_LUTA = 80;
 
-    public double getPlano() {
-        return plano;
-    }
+	public Plano() {
+		this.plano = 0;
+		this.valorPlano = 0.0;
+	}
+	
+	public int getPlano() {
+		return plano;
+	}
 
-    public void setPlano(int plano) {
-        this.plano = plano;
-    }
+	public void setPlano(int plano) {
+		this.plano = plano;
+	}
 
-    public double getValor() {
-        return valor;
-    }
+	public double getValorPlano() {
+		return valorPlano;
+	}
 
-    public void setValor(int plano) {
+	public double setValorPlano(int plano) {
 
-        switch (plano) {
-            case 1:
-                this.valor = PLANO_FAMILIA;
-                break;
-            case 2:
-                this.valor = PLANO_LUTA;
-                break;
-            case 3:
-                this.valor = PLANO_NATACAO;
-                break;
-            case 4:
-                this.valor = PLANO_PADRAO;
-                break;
-            case 5:
-                this.valor = PLANO_UNIVERSITARIO;
-                break;
+		switch (plano) {
+		case 1:
+			this.valorPlano = PLANO_FAMILIA;
+			break;
+		case 2:
+			this.valorPlano = PLANO_UNIVERSITARIO;
+			break;
+		case 3:
+			this.valorPlano = PLANO_PADRAO;
+			break;
+		case 4:
+			this.valorPlano = PLANO_NATACAO;
+			break;
+		case 5:
+			this.valorPlano = PLANO_LUTA;
+			break;
 
-        }
-    }
+		}
+		return valorPlano;
+	}
+	
+	public String nomePlano(int plano) {
+		String opcao = "";
+		switch (plano) {
+		case 1:
+			opcao = "Plano Família";
+			break;
+		case 2:
+			opcao = "Plano Universitário";
+			break;
+		case 3:
+			opcao= "Plano Padrão";
+			break;
+		case 4:
+			opcao = "Plano Padrão";
+			break;
+		case 5:
+			opcao = "Plano Universitário";
+			break;
 
+		}
+		return opcao;
+	}
 }
