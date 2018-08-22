dd21d03c27c29d2b96a7d55a819631677181c972
unknown <hugosilvamartins123@gmail.com>
2018-08-22 20:55:13 -0300

    Criação da classe de excessão para o método clientePagar

---
+++ Academia/src/cpfNaoExisteException.java
@@ -0,0 +1,10 @@
+
+public class cpfNaoExisteException extends Exception {
+     public cpfNaoExisteException() {
+        super();
+    }
+
+    public cpfNaoExisteException(String msgErro) {
+        super(msgErro);
+    }
+}
