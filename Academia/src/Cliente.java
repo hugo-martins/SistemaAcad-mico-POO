67044636f809088dc41b08dc961f4b0029c4d802
Alisson Victor <31294268+alissonvgs@users.noreply.github.com>
2018-08-22 16:43:08 -0300

    Atualização na lista de info. do cliente

--- Academia/src/Cliente.java
+++ Academia/src/Cliente.java
@@ -4,35 +4,49 @@
  * @author Will
  */
 public class Cliente extends Pessoa {
-    private Plano plano;//Plano que o cliente ira contratar
-    private boolean pagamento;
+	private Plano plano;// Plano que o cliente ira contratar
+	private boolean pagamento;
 
-    public Cliente() {
-    }
+	public Cliente() {
+	}
 
-    public Cliente(int id, String nome, String cpf, Contato contato, Endereco endereco, String data_nascimento, Plano plano) {
-        super(id, nome, cpf, contato, endereco, data_nascimento);
-        this.plano = plano;
-    }
+	public Cliente(int id, String nome, String cpf, Contato contato, Endereco endereco, String data_nascimento,
+			Plano plano) {
+		super(id, nome, cpf, contato, endereco, data_nascimento);
+		this.plano = plano;
+	}
 
-    public void setPlano(Plano plano) {//pega o plano contratado pelo cliente
-        this.plano = plano;
-    }
+	public void setPlano(Plano plano) {// pega o plano contratado pelo cliente
+		this.plano = plano;
+	}
 
-    public Plano getPlano() {//retorna o plano contratado pelo cliente
-        return this.plano;
-    }
+	public Plano getPlano() {// retorna o plano contratado pelo cliente
+		return this.plano;
+	}
 
-    public boolean isPagamento(){//retorna o pagamento
-        return pagamento;
-    }
+	public boolean isPagamento() {// retorna o pagamento
+		return pagamento;
+	}
 
-    public void setPagamento(boolean b){//muda o pagamento
-        this.pagamento = b;
-    }
+	public void setPagamento(boolean b) {// muda o pagamento
+		this.pagamento = b;
+	}
 
-    public String toString() {//retorna os dados do cliente
-        return "\nID: " + super.getId() + "\nNome: " + super.getNome() + "\nCPF: " + super.getCpf() + "\nContato: " + super.getContato().toString() + "\nEndereco: " + super.getEndereco() + "\nPlano contratado: " + this.plano.nomePlano(plano.getPlano()) + "\nValor do plano: " + plano.getValorPlano();
-    }
+	public String toString() {// retorna os dados do cliente
+		return "\nID: " + super.getId() +
+				"\nNome: " + super.getNome() +
+				"\nCPF: " + super.getCpf() +
+				"\n==Contato=="+
+				"\nEmail:  " + super.getContato().getEmail() +
+				"\nTelefone: ("+ super.getContato().getTelefone().getDd() + ") " + super.getContato().getTelefone().getNumero() + ")"+
+				"\n==Endereco=="+
+				"\nEstado: " + super.getEndereco().getEstado() + 
+				"\nCidade: " + super.getEndereco().getCidade() +
+				"\nLogradouro: " + super.getEndereco().getLogradouro() +
+				"\nComplemento: " + super.getEndereco().getComplemento() +
+				"\nNúmero: " +  super.getEndereco().getNumero() +
+				"== Plano ==" +
+				"\nPlano contratado: " + this.plano.nomePlano(plano.getPlano()) +
+				"\nValor do plano: " + this.plano.getValorPlano();
+	}
 }
-
